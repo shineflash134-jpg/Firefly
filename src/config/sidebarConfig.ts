67377ca7f98087aa642b33dfb550eee4700a634b
shineflash134-jpg <shineflash134@gmail.com>
2026-06-06 21:15:14 +0800

    更新内容

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -22,7 +22,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	// 当position为left时开启此项，文章详情页将额外显示右侧边栏
 	// 当position为right时开启此项，文章详情页将额外显示左侧边栏
 	// 适用在只想用单侧栏，但在文章详情页想用对侧栏的目录等组件的场景
-	showBothSidebarsOnPostPage: true,
+	showBothSidebarsOnPostPage: flase,
 
 	// 左侧边栏组件配置列表
 	// 组件的渲染顺序完全取决于它们在配置数组中出现的顺序，但top的组件会优先于sticky位置的组件渲染
