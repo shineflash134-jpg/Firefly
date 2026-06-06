8baff22176ef62e4c7397bd5cb02085339274239
shineflash134-jpg <shineflash134@gmail.com>
2026-06-06 20:47:44 +0800

    更新内容

--- src/config/sidebarConfig.ts
+++ src/config/sidebarConfig.ts
@@ -22,7 +22,7 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 	// 当position为left时开启此项，文章详情页将额外显示右侧边栏
 	// 当position为right时开启此项，文章详情页将额外显示左侧边栏
 	// 适用在只想用单侧栏，但在文章详情页想用对侧栏的目录等组件的场景
-	showBothSidebarsOnPostPage: true,
+	showBothSidebarsOnPostPage: false,
 
 	// 左侧边栏组件配置列表
 	// 组件的渲染顺序完全取决于它们在配置数组中出现的顺序，但top的组件会优先于sticky位置的组件渲染
@@ -38,11 +38,11 @@ export const sidebarLayoutConfig: SidebarLayoutConfig = {
 			// 组件类型：用户资料组件
 			type: "profile",
 			// 是否启用该组件
-			enable: false,
+			enable: true,
 			// 组件位置
 			position: "top",
 			// 是否在文章详情页显示
-			showOnPostPage: false,
+			showOnPostPage: true,
 		},
 		{
 			// 组件类型：公告组件
